c9ec17311314bbffd53f1bd25c52c1882ef3dd8a
Mouthieu <mathieu.bossart@imt-atlantique.net>
2025-02-26 17:22:43 +0100

    Initialisation  de l'environnement

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Bureau/tp_student_bot/tp_bot/src
+# Build directory: /home/user/Bureau/tp_student_bot/tp_bot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/user/Bureau/tp_student_bot/tp_bot/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/user/Bureau/tp_student_bot/devel;/home/user/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/user/Bureau/tp_student_bot/devel;/home/user/catkin_ws/devel;/opt/ros/melodic' != '' else []
